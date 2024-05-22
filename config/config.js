444b0b507e9613861783449cbe4af74e355b49a9
wsmr9 <wayner.moya.ramirez@gmail.com>
2024-05-22 10:38:36 -0600

    modified again dockerfile

--- config/config.js
+++ config/config.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
-  port: process.env.PORT || 3000,
   dbUrl: process.env.DATABASE_URL,
-  apiKey : process.env.API_KEY,
   dialect: process.env.DB_DIALECT
 }
 
