2bdd0898eab5e7373e2455c8b2e83736f5e2e2ac
wsmr9 <wayner.moya.ramirez@gmail.com>
2024-05-23 11:21:03 -0600

    modified one column type

--- init-db.sql
+++ init-db.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR(255) NOT NULL,
     image VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
+    price FLOAT(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
